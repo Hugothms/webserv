16650e321af2ff244ccca0f2d5a4609953b35d20
Hugothms <hugothms@gmail.com>
2021-07-06 11:41:20 +0200

    example select

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,15 +6,37 @@
 /*   By: hthomas <hthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/25 13:31:42 by hthomas           #+#    #+#             */
-/*   Updated: 2021/07/06 11:21:00 by hthomas          ###   ########.fr       */
+/*   Updated: 2021/07/06 11:40:19 by hthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "webserv.hpp"
 
-int main(int argc, char const *argv[])
+int main(void)
 {
-	(void) argc;
-	(void) argv;
+    fd_set rfds;
+    struct timeval tv;
+    int retval;
+
+   /* Watch stdin (fd 0) to see when it has input. */
+    FD_ZERO(&rfds);
+    FD_SET(0, &rfds);
+
+   /* Wait up to five seconds. */
+    tv.tv_sec = 5;
+    tv.tv_usec = 0;
+
+   retval = select(1, &rfds, NULL, NULL, &tv);
+    /* Don't rely on the value of tv now! */
+
+   if (retval == -1)
+        perror("select()");
+    else if (retval)
+        printf("Data is available now.\n");
+        /* FD_ISSET(0, &rfds) will be true. */
+    else
+        printf("No data within five seconds.\n");
+
+   exit(EXIT_SUCCESS);
 	return 0;
 }
