f5f35ee028d39a82dba190a46b2349fc67b78903
Hugothms <hugothms@gmail.com>
2021-07-06 11:41:20 +0200

    example select

--- srcs/webserv.hpp
+++ srcs/webserv.hpp
@@ -6,13 +6,16 @@
 /*   By: hthomas <hthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/06 11:00:07 by hthomas           #+#    #+#             */
-/*   Updated: 2021/07/06 11:00:10 by hthomas          ###   ########.fr       */
+/*   Updated: 2021/07/06 11:40:23 by hthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WEBSERV_HPP
 # define WEBSERV_HPP
 
-
+# include <iostream>
+# include <cstring>
+# include <sys/socket.h>
+# include <sys/select.h>
 
 #endif
