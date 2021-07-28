563b89db5e2296a3440de53be1a88929b6998555
Enzo Dal Cerro <gmx1pt@gmail.com>
2021-07-28 16:21:20 +0200

    It kinda works

--- srcs/server.cpp
+++ srcs/server.cpp
@@ -6,7 +6,7 @@
 /*   By: edal--ce <edal--ce@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/28 14:04:40 by edal--ce          #+#    #+#             */
-/*   Updated: 2021/07/28 15:58:46 by edal--ce         ###   ########.fr       */
+/*   Updated: 2021/07/28 16:20:40 by edal--ce         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "server.hpp"
@@ -17,23 +17,21 @@ Server::Server()
 	// port = 8080;
 
 	
-	//Creating a socket on IPv4 mode, using TCP/IP protocol
+	
 	int opt = 1;
-	// listen_socket = ;
+	//Creating a socket on IPv4 mode, using TCP/IP protocol
 	if ((listen_socket = socket(V4, TCP, IP)) == 0)
 	{
 		std::cerr << "socket init failed !\n";
 		exit(1);
 	}
-	
 	if (setsockopt(listen_socket, SOL_SOCKET, SO_REUSEADDR /*| SO_REUSEPORT*/, &opt, sizeof(opt)))
 	{
 		std::cerr << "setsockopt failed !\n";
 		exit(1);	
 	}
-
 	//IPV4 mode, use sockaddr_in6 for V6
-	hint.sin_family = AF_INET;
+	hint.sin_family = V4;
 	
 	//Bind to any IP ont the machine
 	hint.sin_addr.s_addr = INADDR_ANY;
@@ -41,16 +39,13 @@ Server::Server()
 	//HTONS = Host To Network Short | NTOHS is the reverse
 	hint.sin_port = htons(8080);
 	
-
-
-	
-	
-
+	//Bind it ?
 	if (bind(listen_socket, ( struct sockaddr*) &hint, sizeof(hint)) < 0)
 	{
 		std::cerr << "socket bind failed !\n";
 		exit(1);
 	}
+	//Set it to listen
 	if (listen(listen_socket, SOMAXCONN) == -1)
 	{
 		std::cerr << "Can't listen !\n";
@@ -74,7 +69,8 @@ void Server::s_listen()
 	int query_size = read(send_socket, buffer, 1024);
 
 	write(1, buffer, query_size);
-	send(send_socket, "HELLO WORLD\n", 12, 0);
+	send(send_socket, hello, strlen(hello), 0);
+	close(send_socket);
 
 	
 }
