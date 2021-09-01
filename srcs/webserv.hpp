536acb2f5e8ba6ff3adb8362aecd614d877a7226
Hugothms <hugothms@gmail.com>
2021-09-01 16:27:24 +0200

    repeat requests + basic address handling + debug messages

--- srcs/webserv.hpp
+++ srcs/webserv.hpp
@@ -6,7 +6,7 @@
 /*   By: hthomas <hthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/06 11:00:07 by hthomas           #+#    #+#             */
-/*   Updated: 2021/07/09 16:32:15 by hthomas          ###   ########.fr       */
+/*   Updated: 2021/09/01 14:28:29 by hthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@
 #endif
 
 # include <iostream>
+# include <fstream>
 # include <string>
 # include <sys/types.h>
 # include <sys/select.h>
