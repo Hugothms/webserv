13c0eb9a824d8b99de155c47cf78db907b1882ca
Hugothms <hugothms@gmail.com>
2021-09-16 11:07:38 +0200

    cleanning

--- incl/request.hpp
+++ incl/request.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   request.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: edal--ce <edal--ce@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hthomas <hthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/03 14:24:05 by edal--ce          #+#    #+#             */
-/*   Updated: 2021/08/17 16:43:59 by edal--ce         ###   ########.fr       */
+/*   Updated: 2021/09/16 11:05:37 by hthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,15 @@ class Request
 	private :
 		std::string type;		//GET OR POST
 		std::string target;		//THE URL REQUESTED
-		std::string user_agent;	//Info about the platform	
+		std::string user_agent;	//Info about the platform
 		std::string accept;		//List of accepted stuff
-		int 		socket;
-		// const char *hello = "HTTP/1.1 200 OK\nContent-Length: 52\nContent-Type: text/html\nConnection: Closed\n\n<html>\n<body>\n<h1>Hello, World!</h1>\n</body>\n</html>\n";
+		int			socket;
+
 	public :
 		Request();
 		Request(char *buffer, int size, int socket);
 		~Request();
 		void respond(void);
 };
- 
+
 #endif
