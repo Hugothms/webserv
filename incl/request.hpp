ef67d02cce9b036d03e511e8ce6f8f431f8d04b0
Hugothms <hugothms@gmail.com>
2021-09-18 08:01:06 +0200

    content_type content_length

--- incl/request.hpp
+++ incl/request.hpp
@@ -6,7 +6,7 @@
 /*   By: hthomas <hthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/03 14:24:05 by edal--ce          #+#    #+#             */
-/*   Updated: 2021/09/16 16:14:54 by hthomas          ###   ########.fr       */
+/*   Updated: 2021/09/17 17:09:39 by hthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,16 @@
 class Request
 {
 	private :
-		std::string	type;		// GET or POST or DELETE
-		std::string host;		// Name of the server (here localhost)
-		std::string target;		// The URL requested
-		std::string user_agent;	// Info about the platform
-		std::string accept;		// List of accepted stuff
-		int			socket;		// Socket
-		std::string	body;		// Empty for GET
+		std::string		type;			// GET or POST or DELETE
+		std::string		host;			// Name of the server (here localhost)
+		std::string		target;			// The URL requested
+		std::string		user_agent;		// Info about the platform
+		std::string		accept;			// List of accepted stuff
+		int				socket;			// Socket
+		std::string		content_type;	// Only for POST
+		unsigned int	content_length;	// Only for POST
 
-
+		std::string		body;			// Only for POST
 		// HEADERS FIELDS:
 		// Accept-Charset
 		// Accept-Encoding
