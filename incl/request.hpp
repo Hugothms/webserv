c6f361398bb8cbd24d1e75b6c40404aed390f442
Hugothms <hugothms@gmail.com>
2021-09-20 15:35:43 +0200

    add a looot of headers

--- incl/request.hpp
+++ incl/request.hpp
@@ -6,7 +6,7 @@
 /*   By: hthomas <hthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/03 14:24:05 by edal--ce          #+#    #+#             */
-/*   Updated: 2021/09/20 13:53:45 by hthomas          ###   ########.fr       */
+/*   Updated: 2021/09/20 15:32:49 by hthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,32 +22,44 @@ class Request
 		std::string		target;			// The URL requested
 		// std::map<std::string, std::string>	headers;
 		std::string		host;			// Name of the server (here localhost)
+		int				socket;			// Socket
 		std::string		user_agent;		// Info about the platform
 		std::string		accept;			// List of accepted stuff
-		int				socket;			// Socket
+		std::string		accept_language;
+		std::string		accept_encoding;
+		std::string		connection;
+		std::string		upgrade_insecure_requests;
+		std::string		sec_fetch_dest;
+		std::string		sec_fetch_mode;
+		std::string		sec_fetch_site;
+		std::string		sec_fetch_user;
+		std::string		sec_gpc;
+		std::string		cache_control;
 		std::string		content_type;	// Only for POST
 		unsigned int	content_length;	// Only for POST
 
 		std::string		body;			// Only for POST
-		// HEADERS FIELDS:
-		// Accept-Charset
-		// Accept-Encoding
-		// Accept-Language
-		// Authorization
-		// Expect
-		// From
-		// If-Match
-		// If-Modified-Since
-		// If-None-Match
-		// If-Range
-		// If-Unmodified-Since
-		// Max-Forwards
-		// Proxy-Authorization
-		// Range
-		// Referer
-		// TE
-		// User-Agent
 
+		/* Ignored because not HTTP/1.1 compliant
+		DNT
+		*/
+
+		/* Unimplemented headers for now:
+		Accept-Charset
+		Authorization
+		Expect
+		From
+		If-Match
+		If-Modified-Since
+		If-None-Match
+		If-Range
+		If-Unmodified-Since
+		Max-Forwards
+		Proxy-Authorization
+		Range
+		Referer
+		TE
+		*/
 
 	public :
 		Request();
