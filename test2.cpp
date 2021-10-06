f0a5ba9d0468b9616367cde8901736867a62aedb
Enzo Dal Cerro <edal--ce@student.42.fr>
2021-10-06 17:22:20 +0200

    Go

--- test2.cpp
+++ test2.cpp
@@ -6,7 +6,7 @@
 /*   By: edal--ce <edal--ce@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/05 18:59:50 by edal--ce          #+#    #+#             */
-/*   Updated: 2021/10/06 00:27:07 by edal--ce         ###   ########.fr       */
+/*   Updated: 2021/10/06 17:21:31 by edal--ce         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,62 +45,101 @@ int main()
 		return -2;
 	}
 
+	listen(listen_sock, SOMAXCONN);
 
-	fd_set master;
+	fd_set master; 
+	fd_set copy;
 
+	//Add the listening new conn socket to the watched set
 	FD_ZERO(&master);
-
 	FD_SET(listen_sock, &master);
-
-	while(true)
+	
+	int high_sock = listen_sock;
+	while (1)
 	{
-		fd_set copy = master;
+		copy = master;
 
-		int socketCount = select(0, &copy, 0, 0, 0);
-
-		for(int i = 0; i < socketCount; i++)
+		int sock_count = select(high_sock + 1, &copy, nullptr, nullptr, nullptr);
+		//The sockets will be stocked in copy
+		if (FD_ISSET(listen_sock, &copy))
 		{
-			int sock = copy.fd_array[i];
-			if (sock == listen_sock)
-			{
-				//Accept new conn
-				int client = accept(listen_sock, 0, 0);
-				//Add it to list of connected clients
-				FD_SET(client, &master);
-				//Send welcome
-				std::string test = "Hello world";
-				send(client, test.c_str(), test.size() + 1, 0);
-			}
-			else
-			{
-				char buff[4096];
-				memset(buff, 4096, 0);
-				int bytesIn = recv(sock, buf, 4096, 0);
-				if (bytesIn <= 0)
-				{
-					//Drop client
-					close(sock);
-					FD_CLR(sock, &master);
-				}
-				else
-				{
-					for (int i =0 ; i < master.fd_count; i++)
-					{
-						int outsock = master.fd_array[i];
-						if (outsock != listen_sock && outsock != sock)
-						{
-							send(outsock, buff, bytesIn, 0);
-						}
-					}	
-				}
-				//Accept message from established conn
-				//Send response to clients and not sockets
-			}
+			std::cout << "New conn incomming, need to accept it !\n";
+			
+			struct sockaddr_in client_addr;
+			memset(&client_addr, 0, sizeof(client_addr));
+			socklen_t client_len = sizeof(client_addr);
+			
+			int new_client = accept(listen_sock, (struct sockaddr *)&client_addr, &client_len);
+
+			char client_ipv4_str[INET_ADDRSTRLEN];
+			inet_ntop(AF_INET, &client_addr.sin_addr, client_ipv4_str, INET_ADDRSTRLEN);
+			printf("Incoming connection from %s:%d.\n", client_ipv4_str, client_addr.sin_port);
+			//Accept conn
 		}
+
+		// for (int i = 0; i < sock_count; i++)
+		// {
+		// 	//Accept conn
+		// 	if (i == listen_sock)
+		// 	//Receive message
+		// }
 	}
 
+	// fd_set master;
 
-	// if (listen(listen_sock, SOMAXCONN) == -1)
+	// FD_ZERO(&master);
+
+	// FD_SET(listen_sock, &master);
+
+	// while(true)
+	// {
+	// 	fd_set copy = master;
+
+	// 	int socketCount = select(0, &copy, 0, 0, 0);
+
+		// for(int i = 0; i < socketCount; i++)
+		// {
+		// 	int sock = copy.fd_array[i];
+		// 	if (sock == listen_sock)
+		// 	{
+		// 		//Accept new conn
+		// 		int client = accept(listen_sock, 0, 0);
+		// 		//Add it to list of connected clients
+		// 		FD_SET(client, &master);
+		// 		//Send welcome
+		// 		std::string test = "Hello world";
+		// 		send(client, test.c_str(), test.size() + 1, 0);
+		// 	}
+		// 	else
+		// 	{
+		// 		char buff[4096];
+		// 		memset(buff, 4096, 0);
+		// 		int bytesIn = recv(sock, buf, 4096, 0);
+		// 		if (bytesIn <= 0)
+		// 		{
+		// 			//Drop client
+		// 			close(sock);
+		// 			FD_CLR(sock, &master);
+		// 		}
+		// 		else
+		// 		{
+		// 			for (int i =0 ; i < master.fd_count; i++)
+		// 			{
+		// 				int outsock = master.fd_array[i];
+		// 				if (outsock != listen_sock && outsock != sock)
+		// 				{
+		// 					send(outsock, buff, bytesIn, 0);
+		// 				}
+		// 			}	
+		// 		}
+		// 		//Accept message from established conn
+		// 		//Send response to clients and not sockets
+		// 	}
+		// }
+	// }
+
+
+	// if ( == -1)
 	// {
 	// 	std::cerr << "can't listen\n";
 	// 	return -3;
@@ -160,7 +199,7 @@ int main()
 	// 	send(clientsocket, buff, bytes_rcv + 1, 0);
 	// }
 
-	close(clientsocket);
+	// close(clientsocket);
 	close(listen_sock);
 	//Mark socket for listen
 	//Accept a call
