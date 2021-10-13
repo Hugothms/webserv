54bde0f6d18be5529407d13fa22034532d651fe2
Enzo Dal Cerro <edal--ce@student.42.fr>
2021-10-13 18:20:25 +0200

    multi serv

--- srcs/server.cpp
+++ srcs/server.cpp
@@ -3,17 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   server.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hthomas <hthomas@student.42.fr>            +#+  +:+       +#+        */
+/*   By: edal--ce <edal--ce@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/28 14:04:40 by edal--ce          #+#    #+#             */
-/*   Updated: 2021/10/13 17:27:21 by hthomas          ###   ########.fr       */
+/*   Updated: 2021/10/13 18:09:43 by edal--ce         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "server.hpp"
 
 Server::Server()
-: host("localhost"), port(80), root("website"), index("index.html"), max_client_body_size(2048), _setup(false)
+: host("localhost"), port(4242), root("website"), index("index.html"), max_client_body_size(2048), _setup(false)
 {}
 
 Server::Server(	list<Location>	locations,
@@ -48,7 +48,7 @@ Client Server::handle_new_conn()
 	new_client.fd = accept(listen_fd, new_client.get_sockaddr(), new_client.get_addr_len());
 
 	inet_ntop(AF_INET, &(new_client.client_addr.sin_addr), new_client.client_ipv4_str, INET_ADDRSTRLEN);
-	// printf("Incoming connection from %s:%d.\n", new_client.v4str(), new_client.client_addr.sin_port);
+	printf("Incoming connection from %s:%d.\n", new_client.v4str(), new_client.client_addr.sin_port);
 	DEBUG("Client created !");
 	// clients.push_back(new_client);
 	return (new_client);
@@ -80,73 +80,72 @@ int Server::setup(void)
 
 	listen(listen_fd, SOMAXCONN);
 
-	FD_ZERO(&master_set);
+	// FD_ZERO(&master_set);
 
-	FD_SET(listen_fd, &master_set);
+	// FD_SET(listen_fd, &master_set);
 
 	_setup = true;
 	high_fd = listen_fd;
-	return 0;
+	return listen_fd;
 }
-
 int Server::run(void)
 {
-	if (_setup == false)
-		setup();
-	while(true)
-	{
-		copy_set = master_set;
+	// if (_setup == false)
+	// 	setup();
+	// while(true)
+	// {
+	// 	copy_set = master_set;
 
-		//This can be optimized
-		for (vector<Client>::iterator i = _clients.begin(); i != _clients.end(); i++)
-		{
-			if (i->fd > high_fd)
-				high_fd = i->fd;
-			else if (i->fd == -1)
-			{
-				i = _clients.erase(i);
-				if (i == _clients.end())
-					break;
-			}
-		}
+	// 	//This can be optimized
+	// 	for (vector<Client>::iterator i = _clients.begin(); i != _clients.end(); i++)
+	// 	{
+	// 		if (i->fd > high_fd)
+	// 			high_fd = i->fd;
+	// 		else if (i->fd == -1)
+	// 		{
+	// 			i = _clients.erase(i);
+	// 			if (i == _clients.end())
+	// 				break;
+	// 		}
+	// 	}
 
-		select(high_fd + 1, &copy_set, NULL, NULL, NULL);
-		if (FD_ISSET(listen_fd, &copy_set))
-		{
-			Client tmp = handle_new_conn();
-			_clients.push_back(tmp);
-			DEBUG("Client added to the list: ");
-			// _clients.back().identify();
-			//Add the client FD to master for processing
-			FD_SET(tmp.fd, &master_set);
-			DEBUG("Client added to the FD_SET: ");
-		}
-		// //Loop through all the clients and find out if they sent
-		for (vector<Client>::iterator i = _clients.begin(); i != _clients.end(); i++)
-		{
-			if (FD_ISSET(i->fd, &copy_set))
-			{
-				char buff[BUFFER_SIZE];
-				int received_count = recv(i->fd, buff, BUFFER_SIZE, 0);
-				if (received_count <= 0)
-				{
-					DEBUG("Client is done : ");
-					i->identify();
-					DEBUG("\n");
-					FD_CLR(i->fd, &master_set);
-					close(i->fd);
-					i->fd = -1;
-				}
-				else
-				{
-					Request req(buff,received_count, i->fd);
-					// DEBUG(buff);
-					req.respond();
-				}
+	// 	select(high_fd + 1, &copy_set, NULL, NULL, NULL);
+	// 	if (FD_ISSET(listen_fd, &copy_set))
+	// 	{
+	// 		Client tmp = handle_new_conn();
+	// 		_clients.push_back(tmp);
+	// 		DEBUG("Client added to the list: ");
+	// 		// _clients.back().identify();
+	// 		//Add the client FD to master for processing
+	// 		FD_SET(tmp.fd, &master_set);
+	// 		DEBUG("Client added to the FD_SET: ");
+	// 	}
+	// 	// //Loop through all the clients and find out if they sent
+	// 	for (vector<Client>::iterator i = _clients.begin(); i != _clients.end(); i++)
+	// 	{
+	// 		if (FD_ISSET(i->fd, &copy_set))
+	// 		{
+	// 			char buff[BUFFER_SIZE];
+	// 			int received_count = recv(i->fd, buff, BUFFER_SIZE, 0);
+	// 			if (received_count <= 0)
+	// 			{
+	// 				DEBUG("Client is done : ");
+	// 				i->identify();
+	// 				DEBUG("\n");
+	// 				FD_CLR(i->fd, &master_set);
+	// 				close(i->fd);
+	// 				i->fd = -1;
+	// 			}
+	// 			else
+	// 			{
+	// 				Request req(buff,received_count, i->fd);
+	// 				// DEBUG(buff);
+	// 				req.respond();
+	// 			}
 
-			}
-		}
-	}
+	// 		}
+	// 	}
+	// }
 	// close(listen_fd);
 }
 
@@ -154,7 +153,10 @@ unsigned int	Server::get_port()
 {
 	return port;
 }
-
+int Server::get_listen_fd(void)
+{
+	return listen_fd;
+}
 void	Server::set_locations(list<Location> locations)
 {
 	this->locations = locations;
