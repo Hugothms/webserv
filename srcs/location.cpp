870b0d9144d35c67e833099ba8c459846e4931dc
Hugo Thomas <hugothms@gmail.com>
2021-10-13 13:29:46 +0200

    clean includes

--- srcs/location.cpp
+++ srcs/location.cpp
@@ -6,7 +6,7 @@
 /*   By: hthomas <hthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/28 14:04:40 by edal--ce          #+#    #+#             */
-/*   Updated: 2021/10/12 21:08:05 by hthomas          ###   ########.fr       */
+/*   Updated: 2021/10/13 13:23:56 by hthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,11 @@ Location::~Location() {}
 
 Location::Location() {}
 
-Location::Location(	string				location,
+Location::Location(	string			location,
 					list<string>	HTTP_methods,
-					string				HTTP_redirection,
-					string				location_root,
-					bool 					directory_listing,
-					string				default_answer)
+					string			HTTP_redirection,
+					string			location_root,
+					bool 			directory_listing,
+					string			default_answer)
 	: location(location), HTTP_methods(HTTP_methods), HTTP_redirection(HTTP_redirection), location_root(location_root), directory_listing(directory_listing), default_answer(default_answer)
-{
-}
+{}
