b42c153fe7bfe60d2f38b2d04889953c729a0a1e
Hugo Thomas <hugothms@gmail.com>
2021-10-13 12:50:42 +0200

    organise includes

--- incl/webserv.hpp
+++ incl/webserv.hpp
@@ -6,7 +6,7 @@
 /*   By: hthomas <hthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/08 14:55:13 by edal--ce          #+#    #+#             */
-/*   Updated: 2021/10/12 21:08:05 by hthomas          ###   ########.fr       */
+/*   Updated: 2021/10/13 12:47:10 by hthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,6 @@
 
 # include "includes.hpp"
 # include "server.hpp"
-
-# if DEBUG_ACTIVE == 1
-#  define DEBUG(x) cout << x << endl;
-# else
-#  define DEBUG(x)
-# endif
 
 class Server;
 
