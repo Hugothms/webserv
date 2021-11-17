ef1c97b5113186c37800e0803f0ffdad8e8a4548
Hugothms <hugothms@gmail.com>
2021-11-17 16:02:16 +0100

    refacto

---
+++ srcs/utils_header.cpp
@@ -0,0 +1,131 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils_header.cpp                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hthomas <hthomas@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/17 15:43:40 by hthomas           #+#    #+#             */
+/*   Updated: 2021/11/17 15:46:59 by hthomas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "request.hpp"
+
+string	getdayofweek(const int day)
+{
+	switch(day)
+	{
+		case 1:
+			return "Mon";
+		case 2:
+			return "Tue";
+		case 3:
+			return "Wed";
+		case 4:
+			return "Thu";
+		case 5:
+			return "Fri";
+		case 6:
+			return "Sat";
+		case 0:
+			return "Sun";
+		default :
+			return "UNDEFINED_DAY";
+	}
+}
+
+string	getmonth(const int month)
+{
+	switch(month)
+	{
+		case 0:
+			return "Jan";
+		case 1:
+			return "Feb";
+		case 2:
+			return "Mar";
+		case 3:
+			return "Apr";
+		case 4:
+			return "May";
+		case 5:
+			return "Jun";
+		case 6:
+			return "Jul";
+		case 7:
+			return "Aug";
+		case 8:
+			return "Sep";
+		case 9:
+			return "Oct";
+		case 10:
+			return "Nov";
+		case 11:
+			return "Dec";
+		default :
+			return "UNDEFINED_MONTH";
+	}
+}
+
+string	get_time_stamp()
+{
+	time_t now = time(0);
+
+	tm *time = gmtime(&now);
+	stringstream output;
+	output << getdayofweek(time->tm_wday);
+
+	if (time->tm_mday < 10)
+		output  << ", 0" << time->tm_mday;
+	else
+		output  << ", " << time->tm_mday;
+	output << " " << getmonth(time->tm_mon);
+	output << " " << time->tm_year + 1900 << " ";
+	if (time->tm_hour < 10)
+		output << "0";
+	output << time->tm_hour << ":";
+	if (time->tm_min < 10)
+		output << "0";
+	output << time->tm_min << ":";
+	if (time->tm_sec < 10)
+		output << "0";
+	output << time->tm_sec << " GMT";
+
+	return (output.str());
+}
+
+string	get_type(const string &str)
+{
+	string ret;
+	if (str.find(".png", str.length() - 4) != string::npos)
+		ret = "image/png";
+	else if (str.find(".jpg", str.length() - 4) != string::npos)
+		ret = "image/jpg";
+	else if (str.find(".png", str.length() - 4) != string::npos)
+		ret = "image/x-icon";
+	else if (str.find(".ico", str.length() - 4) != string::npos)
+		ret = "image/x-icon";
+	else if (str.find(".css", str.length() - 4) != string::npos)
+		ret = "text/css";
+	else if (str.find(".js", str.length() - 3) != string::npos)
+		ret = "application/javascript";
+	else if (str.find(".html", str.length() - 5) != string::npos)
+		ret = "text/html";
+	else
+		ret = "text/html";
+	return ret;
+}
+
+string get_header(const string &message, const string &type, const size_t length)
+{
+	stringstream header;
+	header << "HTTP/1.1 " << message << endl;
+	header << "Date: " << get_time_stamp() << endl;
+	header << "Server: webserv/0.01" << endl;
+	header << "Content-Type: " << type << endl;
+	header << "Content-Length: " << length << endl;
+	header << "Connection: Closed" << endl;
+	header << endl;
+	return (header.str());
+}
