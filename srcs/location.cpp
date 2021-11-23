e1b77706a69128a3e39960131747d184aed25571
Hugothms <hugothms@gmail.com>
2021-11-23 08:18:53 +0100

    clean

--- srcs/location.cpp
+++ srcs/location.cpp
@@ -6,7 +6,7 @@
 /*   By: hthomas <hthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/28 14:04:40 by edal--ce          #+#    #+#             */
-/*   Updated: 2021/11/21 19:13:44 by hthomas          ###   ########.fr       */
+/*   Updated: 2021/11/23 08:18:07 by hthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ Location::Location()
 {
 	// map_pointer_function.insert(make_pair("HTTP_redirection", &(Location::set_HTTP_redirection)));
 	// map_pointer_function.insert(make_pair("root", &(Location::set_path)));
-	// map_pointer_function.insert(make_pair("default_answer", &(Location::set_default_answer)));
 	// map_pointer_function.insert(make_pair("index", &(Location::set_index)));
 	// map_pointer_function.insert(make_pair("upload_directory", &(Location::set_upload_directory)));
 }
