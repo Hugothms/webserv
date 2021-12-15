61fa4d347b677886962334efa1c432e76ad34d4b
Enzo <Da Cerro>
2021-12-15 12:11:39 +0100

    fix

--- srcs/request.cpp
+++ srcs/request.cpp
@@ -6,7 +6,7 @@
 /*   By: edal--ce <edal--ce@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/15 17:21:43 by hthomas           #+#    #+#             */
-/*   Updated: 2021/12/15 10:15:29 by edal--ce         ###   ########.fr       */
+/*   Updated: 2021/12/15 12:09:18 by edal--ce         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,8 +58,8 @@ map<unsigned int, string> codes = create_map_return_codes();
 
 Request::~Request()
 {
-	if (this->location)
-		delete(this->location);
+	// if (this->location)
+	// 	delete(this->location);
 	// free(static_cast<void *>(this->location));
 }
 
