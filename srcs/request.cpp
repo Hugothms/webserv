d927f1ce06cd3e4c6e5c23e32ccb43cf7a650ccd
Enzo <Da Cerro>
2021-12-17 18:32:39 +0100

    restored get

--- srcs/request.cpp
+++ srcs/request.cpp
@@ -6,7 +6,7 @@
 /*   By: edal--ce <edal--ce@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/15 17:21:43 by hthomas           #+#    #+#             */
-/*   Updated: 2021/12/15 14:16:52 by edal--ce         ###   ########.fr       */
+/*   Updated: 2021/12/17 18:31:58 by edal--ce         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -206,6 +206,71 @@ char *ft_strdup(string msg)
 	return ret;
 }
 
+char **Request::build_cgi_av(string &extention_name)
+{
+	std::vector<string> av;
+	string server_root = string(getcwd(NULL, 0));
+	string bin_path = server->get_cgis()[extention_name];
+
+	av.push_back(bin_path);
+	av.push_back(server_root + "/" + filepath.substr(0, filepath.find_first_of('?', 0)));
+
+	char **_av = static_cast<char**>(malloc(sizeof(char *) * (av.size() + 1)));
+		
+	for (size_t j = 0; j < av.size(); j++)
+	{
+		_av[j] = ft_strdup(av[j]);
+		// DEBUG("e"<<j << ":" << av[j]);
+	}
+	_av[av.size()] = 0;
+	return _av;
+}
+
+char **Request::build_cgi_env(string &extention_name)
+{
+	std::vector<string> ev;
+	string server_root = string(getcwd(NULL, 0));
+	string newfilepath("/" + filepath.substr(0, filepath.find_first_of('?', 0)));
+
+
+	ev.push_back("GATEWAY_INTERFACE=CGI/1.1");
+	ev.push_back("SERVER_PROTOCOL=HTTP/1.1");
+	ev.push_back("REDIRECT_STATUS=200");
+
+		
+	ev.push_back("HTTP_HOST=" + target.substr(0, target.find_first_of('/', 0)));
+	ev.push_back("HTTP_HOST=" + target.substr(target.find_first_of('/', 0) + 1));
+		
+
+	ev.push_back("REQUEST_METHOD=" + type);
+
+	ev.push_back("SCRIPT_FILENAME=" + server_root + newfilepath);
+	ev.push_back("SCRIPT_NAME=" + newfilepath);
+
+
+	if (type == "GET")
+	{
+		ev.push_back("QUERY_STRING="+ filepath.substr(filepath.find_first_of('?') + 1));
+		ev.push_back("CONTENT_LENGTH=0");//+ to_string(headers["Body"].length()) );
+		extention_name = extention_name.substr(0, extention_name.find_first_of('?'));
+	}
+	else if (type == "POST")
+	{
+		ev.push_back("CONTENT_TYPE=" + content_type);
+		ev.push_back("CONTENT_LENGTH="+ to_string_custom(headers["Body"].size()));//(data_buff->length()) );
+	}
+
+	char **_ev = static_cast<char**>(malloc(sizeof(char *) * (ev.size() + 1)));
+		
+	for (size_t j = 0; j < ev.size(); j++)
+	{
+		_ev[j] = ft_strdup(ev[j]);
+		// DEBUG("e"<<j << ":" << ev[j]);
+	}
+	_ev[ev.size()] = 0;
+	return _ev;
+}
+
 void	Request::launch_cgi(string &body, string extention_name)
 {
 	// look: https://github.com/brokenfiles/webserv/blob/c1601dfad39a04299bc86b165994a87f3146d78d/srcs/classes/cgi/Cgi.cpp addMetaVariables
@@ -217,6 +282,8 @@ void	Request::launch_cgi(string &body, string extention_name)
 		cerr << "cgi: pipe failed" << endl;
 		exit(EXIT_FAILURE);
 	}
+	int n_pip[2];
+	pipe(n_pip);
 	pid_t pid = fork();
 	if (pid < 0)
 	{
@@ -226,81 +293,49 @@ void	Request::launch_cgi(string &body, string extention_name)
 
 	code = 200;
 	//https://stackoverflow.com/questions/33052169/call-php-cgi-from-c-with-request-method-post
+	
 	if (pid == 0)
 	{
 		string server_root = string(getcwd(NULL, 0));
-		// envp[0] = 0;
-		// DEBUG("BODYIS " << headers["Body"]);
-		// DEBUG("OR " << body);
 
-		// envp[1] = ft_strdup("CONTENT_LENGHT=" + to_string(headers["Body"].length()));
-
-		// //This is wrong it seems
-
-
-		// envp[0] = ft_strdup(("DOCUMENT_ROOT=" + server_root));
-		// envp[1] = ft_strdup(("HTTP_HOST=" + (server->get_server_names().front())));
-		// envp[2] = ft_strdup(("SCRIPT_FILENAME=" + server_root + "/" + filepath));
-		// envp[3] = ft_strdup(("SCRIPT_NAME=" + filepath.substr(filepath.find_last_of('/')+ 1)));
-		// envp[4] = ft_strdup(("PATH=" + server_root + "/"));
-		// envp[5] = ft_strdup(("PATH_INFO=" + server_root + "/" + filepath));
-		// envp[6] = 0;
-		std::vector<string> ev;
+		// std::vector<string> ev;
 		std::vector<string> av;
 
-		// int avl = 4;
-		// int envl = 11;
 
-		// char **argv = (char**) malloc(sizeof(char*) * avl--);
-		// char **envp = (char**) malloc(sizeof(char*) * envl--);
+		// ev.push_back("GATEWAY_INTERFACE=CGI/1.1");
+		// ev.push_back("SERVER_PROTOCOL=HTTP/1.1");
+		// ev.push_back("REDIRECT_STATUS=200");
 
-		ev.push_back("GATEWAY_INTERFACE=CGI/1.1");
-		ev.push_back("SERVER_PROTOCOL=HTTP/1.1");
-		ev.push_back("REDIRECT_STATUS=200");
-
-
-		// envp[0] = ft_strdup("GATEWAY_INTERFACE=CGI/1.1");
-		// envp[1] = ft_strdup("SERVER_PROTOCOL=HTTP/1.1");
-		// envp[2] = ft_strdup("REDIRECT_STATUS=200");
-		// envp[3] = ft_strdup("HTTP_HOST=" + target.substr(0, target.find_first_of('/', 0)));
-		// envp[4] = ft_strdup("HTTP_HOST=" + target.substr(target.find_first_of('/', 0) + 1));
-		// envp[5] = ft_strdup("REQUEST_METHOD=" + type);
-		// envp[6] = ft_strdup("SCRIPT_FILENAME=" + server_root + newfilepath);
-		// envp[7] = ft_strdup("SCRIPT_NAME=" + newfilepath);
-		// envp[7] = 0;
-
-		//Error right here
 		
-		ev.push_back("HTTP_HOST=" + target.substr(0, target.find_first_of('/', 0)));
-		ev.push_back("HTTP_HOST=" + target.substr(target.find_first_of('/', 0) + 1));
+		// ev.push_back("HTTP_HOST=" + target.substr(0, target.find_first_of('/', 0)));
+		// ev.push_back("HTTP_HOST=" + target.substr(target.find_first_of('/', 0) + 1));
 		
 
-		ev.push_back("REQUEST_METHOD=" + type);
+		// ev.push_back("REQUEST_METHOD=" + type);
 		
 
 		string newfilepath("/" + filepath.substr(0, filepath.find_first_of('?', 0)));
 
-		ev.push_back("SCRIPT_FILENAME=" + server_root + newfilepath);
-		ev.push_back("SCRIPT_NAME=" + newfilepath);
+		// ev.push_back("SCRIPT_FILENAME=" + server_root + newfilepath);
+		// ev.push_back("SCRIPT_NAME=" + newfilepath);
 		
 
 		// envp[10] = 0;
-		if (type == "GET")
-		{
-			ev.push_back("QUERY_STRING="+ filepath.substr(filepath.find_first_of('?') + 1));
-			ev.push_back("CONTENT_LENGTH=0");//+ to_string(headers["Body"].length()) );
-			extention_name = extention_name.substr(0, extention_name.find_first_of('?'));
-		}
-		else if (type == "POST")
+		// if (type == "GET")
+		// {
+		// 	// ev.push_back("QUERY_STRING="+ filepath.substr(filepath.find_first_of('?') + 1));
+		// 	// ev.push_back("CONTENT_LENGTH=0");//+ to_string(headers["Body"].length()) );
+		// 	extention_name = extention_name.substr(0, extention_name.find_first_of('?'));
+		// }
+		if (type == "POST")
 		{
 			//To change according to content typefor
 			
-			ev.push_back("CONTENT_TYPE=" + content_type);
-			ev.push_back("CONTENT_LENGTH="+ to_string_custom(headers["Body"].size()));//(data_buff->length()) );
+			// ev.push_back("CONTENT_TYPE=" + content_type);
+			// ev.push_back("CONTENT_LENGTH="+ to_string_custom(headers["Body"].size()));//(data_buff->length()) );
 		
 
-			int n_pip[2];
-			pipe(n_pip);
+
 			// for (std::map<string, string>::iterator a = headers.begin(); a != headers.end(); a++)
 			// 	DEBUG("PAIR IS: "<< a->first << "|" << a->second);
 			
@@ -309,34 +344,34 @@ void	Request::launch_cgi(string &body, string extention_name)
 			DEBUG("DATA OK-----------------------------------");
 
 			dup2(n_pip[0], STDIN_FILENO);
-			write(n_pip[1], headers["Body"].c_str(), headers["Body"].size());
+			// write(n_pip[1], headers["Body"].c_str(), headers["Body"].size());
 		}
 	
-		string bin_path = server->get_cgis()[extention_name];
 	
-		av.push_back(bin_path);
-		av.push_back(server_root + "/" + filepath.substr(0, filepath.find_first_of('?', 0)));
-
-		char **_av = static_cast<char**>(malloc(sizeof(char *) * (av.size() + 1)));
-		char **_ev = static_cast<char**>(malloc(sizeof(char *) * (ev.size() + 1)));
-
-		for (size_t j = 0; j < av.size(); j++)
-		{
-			_av[j] = ft_strdup(av[j]);
-			DEBUG("a"<<j << ":" << av[j]);
-		}
+	
 		
-		for (size_t j = 0; j < ev.size(); j++)
-		{
-			_ev[j] = ft_strdup(ev[j]);
-			DEBUG("e"<<j << ":" << ev[j]);
-		}
+		char **_ev = build_cgi_env(extention_name);//static_cast<char**>(malloc(sizeof(char *) * (ev.size() + 1)));
+		char **_av = build_cgi_av(extention_name);// static_cast<char**>(malloc(sizeof(char *) * (av.size() + 1)));
 		
-		_av[av.size()] = 0;
-		_ev[ev.size()] = 0;
+
+		// for (size_t j = 0; j < av.size(); j++)
+		// {
+		// 	_av[j] = ft_strdup(av[j]);
+		// 	DEBUG("a"<<j << ":" << av[j]);
+		// }
+		
+		// for (size_t j = 0; j < ev.size(); j++)
+		// {
+		// 	_ev[j] = ft_strdup(ev[j]);
+		// 	DEBUG("e"<<j << ":" << ev[j]);
+		// }
+		
+		// _av[av.size()] = 0;
+		// _ev[ev.size()] = 0;
 
 		dup2(fdpipe[1], STDOUT_FILENO);
 
+		string bin_path = server->get_cgis()[extention_name];
 		// TODO: must execve php-cgi
 		if (execve(bin_path.c_str(), _av, _ev) < 0)
 			code = 404;
@@ -345,6 +380,11 @@ void	Request::launch_cgi(string &body, string extention_name)
 	}
 	else
 	{
+		if (type == "POST")
+		{
+			write(n_pip[1], headers["Body"].c_str(), headers["Body"].size());
+			close(n_pip[1]);
+		}
 		wait(0);
 		close(fdpipe[1]); // parent doesn't write
 		char reading_buf;
