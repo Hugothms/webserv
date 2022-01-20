fc176e28014987cf08ee040b94d81c6ad8899665
Hugothms <hugothms@gmail.com>
2022-01-20 11:30:34 +0100

    website to www

--- website/demo/site1/myscript.js
+++
@@ -1,4 +0,0 @@
-function myFunction() {
-	// body...
-	alert("POGGERS");
-}
---
+++ www/info.php
@@ -0,0 +1,10 @@
+<?php
+
+// Show all information, defaults to INFO_ALL
+phpinfo(1);
+
+// Show just the module information.
+// phpinfo(8) yields identical results.
+// phpinfo(INFO_MODULES);
+
+?>
